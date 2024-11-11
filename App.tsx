438a626ae808574181f052f96ad14c3991505eb9
TranVanHung_20061461 <hungtran2002a2@gmail.com>
2024-11-11 16:09:27 +0700

    Add initial screens and navigation setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomScreen";
+import ProductsScreen from "./src/screens/ProductsScreen";
+import ProductDetailScreen from "./src/screens/ProductDetailScreen";
+import { StyleSheet } from "react-native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Products"
+          component={ProductsScreen}
+          options={{ title: "The world’s Best Bike" }}
+        />
+        <Stack.Screen
+          name="ProductDetail"
+          component={ProductDetailScreen}
+          options={{ title: "Product Details" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/HomScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HomScreen = () => {
+  return (
+    <View>
+      <Text>HomScreen</Text>
+    </View>
+  );
+};
+
+export default HomScreen;
+
+const styles = StyleSheet.create({});
